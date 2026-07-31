b699153fb59a2400d8c671b57a182a4958919f0b
alexrashidov <rashidov.sasha@yandex.ru>
2026-07-31 15:42:24 +0700

    Fix paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/SVEAK/',
     root: resolve(__dirname, 'src'),
     server: {
         port: 3000,
